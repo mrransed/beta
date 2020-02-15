2ffd9d632da02b24bfcba620aa8c480a417b23af
Andres Moreno <ransedweb@gmail.com>
2020-02-14 20:24:04 -0500

    inicio estructura html

--- controller/bd/usuarios.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE usuariosClientes(
-    id INT,
-    nombre VARCHAR(30),
-    apellido VARCHAR(30),
-    idGenero INT,
-    idCiudad INT,
-    
-
-);
--- index.php
+++ index.php
@@ -1,42 +1,13 @@
- <?php
-
-session_start();
-
-
-if($_SESSION['usuario'])
-	{
-	
-		
-
-	}else
-		{
-			// echo '<META HTTP-EQUIV="REFRESH" CONTENT="0.1;URL=view/modules/login.php">';
-			header("LOCATION:view/login.php");
-		}
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Presty</title>
 </head>
 <body>
-	<?php
-
-		require_once 'controller/session_controller.php';
-		
-		@$pag=$_GET['pag'];
-		// $paginas=new sessionController($pag);
-		// $paginas->paginas();
-		echo $pag;
-
-
-
-
-
-
-	?>
+	<header></header>
+	<section></section>
+	<footer></footer>
 </body>
 </html>
